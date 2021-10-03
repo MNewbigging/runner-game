c0e0b3931cc066221acb28ba36430ec180a6e250
MNewbigging <mrnewbigging@gmail.com>
2021-10-03 15:03:19 +0100

    tweaking values

--- src/components/game/scene/Backdrop.tsx
+++ src/components/game/scene/Backdrop.tsx
@@ -12,12 +12,20 @@ export class Backdrop extends React.Component<Props> {
   public render() {
     return (
       <>
-        {/* <div className={'backdrop bg1'}></div>
-        <div className={'backdrop bg2'}></div>
-        <div className={'backdrop bg3'}></div>
-        <div className={'backdrop bg4'}></div> */}
         <div className={'backdrop'}>
-          <div className={'bg5'}></div>
+          <div className={'bg-image bg1'}></div>
+        </div>
+        <div className={'backdrop'}>
+          <div className={'bg-image bg2'}></div>
+        </div>
+        <div className={'backdrop'}>
+          <div className={'bg-image bg3'}></div>
+        </div>
+        <div className={'backdrop'}>
+          <div className={'bg-image bg4'}></div>
+        </div>
+        <div className={'backdrop'}>
+          <div className={'bg-image bg5'}></div>
         </div>
       </>
     );
