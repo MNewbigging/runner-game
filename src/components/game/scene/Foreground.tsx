8f9609a9e0daa3e562c5677f0430f67ca6d2a198
MNewbigging <mrnewbigging@gmail.com>
2021-10-03 18:23:34 +0100

    foreground pans by speed

--- src/components/game/scene/Foreground.tsx
+++ src/components/game/scene/Foreground.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { observer } from 'mobx-react';
+import React, { CSSProperties } from 'react';
 
 import './foreground.scss';
 
@@ -7,18 +8,33 @@ interface Props {
   playerSpeed?: number;
 }
 
+@observer
 export class Foreground extends React.Component<Props> {
   public render() {
     const { paused, playerSpeed } = this.props;
 
+    const speed = playerSpeed ? this.getScrollSpeed(playerSpeed) : 0;
+
+    const tileStyle: CSSProperties = {
+      animationDuration: speed + 's',
+      animationPlayState: paused ? 'paused' : 'running',
+    };
+
+    const bigTileStyle: CSSProperties = {
+      animationDuration: speed * 2 + 's',
+      animationPlayState: paused ? 'paused' : 'running',
+    };
+
     return (
       <div className={'foreground'}>
-        <div className={'top-row tiles'}></div>
-        <div className={'bot-row big-tiles'}></div>
-        <div className={'bot-row big-tiles'}></div>
+        <div className={'top-row tiles'} style={tileStyle}></div>
+        <div className={'bot-row big-tiles'} style={bigTileStyle}></div>
+        <div className={'bot-row big-tiles'} style={bigTileStyle}></div>
       </div>
     );
   }
 
-  private getScrollSpeed(playerSpeed: number) {}
+  private getScrollSpeed(playerSpeed: number) {
+    return playerSpeed / 2;
+  }
 }
